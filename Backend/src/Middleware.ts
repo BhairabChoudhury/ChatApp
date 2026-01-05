02ac43d8ad6dca0ee42b710fb5d3a632007c57e1
BhairabChoudhury <bhairabchoudhury02@gmail.com>
2026-01-05 15:42:11 +0530

    second

---
+++ Backend/src/Middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction , Request ,Response } from "express";
+import {JWT} from "./config" ;
+import jwt from "jsonwebtoken" 
+export const useMiddleware = async (req:Request ,res :Response)=> {
+
+     const header = req.headers["authorization"] ;
+     const decode = jwt.verify(header as string , JWT) ;
+
+     if(decode) {
+        // @ts-ignore
+         req.userId = decode.id ; 
+         //@ts-ignore
+         next() ;
+     } else {
+                 res.status(401).json({ message: "Unauthorized User" });
+     }
+}
